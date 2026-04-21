97350348222b3f53a058c9ba2d27250bac1ce2d6
nokcha11 <lie8220@naver.com>
2026-04-21 22:27:33 +0900

    💄 DB연동화면 한줄로 나오게 조정 PHP함수정의문

--- study18.php
+++ study18.php
@@ -1,18 +1,18 @@
 <?php
+$arrNames = array('윤겸','규나','민하','은애','윤겸2','규나2','민하2','은애2');
+echo $arrNames[0]."<br>";
+echo $arrNames[1]."<br>";
+echo $arrNames[2]."<br>";
+echo $arrNames[3]."<br>";
+echo $arrNames[4]."<br>";
+echo $arrNames[5]."<br>";
+echo $arrNames[6]."<br>";
+echo $arrNames[7]."<br>";
 
-$arrayName = array('윤겸','규나', '민하', '은애');
 
-// 배열 하나씩 직접 출력
-echo $arrayName[0] . "<br>";
-echo $arrayName[1] . "<br>";
-echo $arrayName[2] . "<br>";
-echo $arrayName[2] . "<br>";
-
-
-
-// 반복문으로 출력
-for($i = 0; $i < count($arrayName); $i++){
-    echo $arrayName[$i] . "<br>";
-}
+for($i=1; $i<count($arrNames); $i++)
+  {
+    echo $arrNames[$i]."<br>";
+  }
 
 ?>

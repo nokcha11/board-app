130a38ef67e1afb8a61e8da81432c3f47e1421e4
nokcha11 <lie8220@naver.com>
2026-05-02 02:01:39 +0900

    💄 벚꽃효과주기 수정

--- todolist/js/sakura.js
+++ todolist/js/sakura.js
@@ -1,16 +1,40 @@
-setInterval(() => {
+/* ================= 벚꽃 생성 효과 ================= */
+function createPetal() {
   const petal = document.createElement("div");
   petal.className = "petal";
 
-  petal.style.width = (14 + Math.random() * 10) + "px";
-  petal.style.height = (20 + Math.random() * 12) + "px";
+  /* 꽃잎 시작 위치 랜덤 */
   petal.style.left = Math.random() * window.innerWidth + "px";
-  petal.style.animationDuration = (5 + Math.random() * 5) + "s";
-  petal.style.transform = `rotate(${Math.random()*360}deg)`;
+
+  /* 꽃잎 크기 랜덤 */
+  const size = 12 + Math.random() * 12;
+  petal.style.setProperty("--size", size + "px");
+
+  /* 떨어지는 속도 랜덤 */
+  const duration = 7 + Math.random() * 6;
+  petal.style.setProperty("--duration", duration + "s");
+
+  /* 투명도 랜덤 */
+  const opacity = 0.45 + Math.random() * 0.35;
+  petal.style.setProperty("--opacity", opacity);
+
+  /* 시작 회전 랜덤 */
+  const startRotate = Math.random() * 360;
+  petal.style.setProperty("--start-rotate", startRotate + "deg");
+
+  /* 바람 방향/세기 랜덤 */
+  const wind = (Math.random() * 260 - 130);
+  petal.style.setProperty("--wind", wind + "px");
 
   document.body.appendChild(petal);
 
   setTimeout(() => {
     petal.remove();
-  }, 10000);
-}, 300);
+  }, duration * 1000);
+}
+
+/* ================= 꽃잎 생성 간격 =================
+   숫자가 작을수록 꽃잎이 많이 나옴
+   추천: 350~600
+*/
+setInterval(createPetal, 450);
